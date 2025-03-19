f9eea26c16e6e329251b11481053da2f5d9ffa60
Adityaraj002 <adityaraj99106@gmail.com>
2025-03-19 19:23:15 +0530

    change: Profile--> userInfo completed

--- src/screens/ui/UserInfo.tsx
+++ src/screens/ui/UserInfo.tsx
@@ -1,4 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -7,7 +14,11 @@ import { fonts } from "../../styles/font";
 import { theme } from "../../styles/theme";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Avatar from "../../components/AvatarBatch";
+import { useNavigation } from "@react-navigation/native";
+import { data } from "../../utils/data";
+import MusicShowcase from "../../components/MusicShowcase";
 const UserInfo = () => {
+  const navigation = useNavigation<any>();
   return (
     <SafeAreaView style={styles.container}>
       {/* header */}
@@ -18,6 +29,7 @@ const UserInfo = () => {
             name="arrow-back-outline"
             style={styles.icons}
             color="black"
+            onPress={() => navigation.goBack()}
           />
           {/* profile text */}
           <Text style={styles.TextOne}>Profile</Text>
@@ -25,11 +37,82 @@ const UserInfo = () => {
         {/* edit icons */}
         <FontAwesome6 name="edit" style={[styles.editIcon]} color="black" />
       </View>
-      {/* Avatar or user Image */}
-      <View style={styles.AvatarContainer} >
-        <Avatar name="user" size={hp(25)} imageUrl={require("../../../assets/boy.jpg")} />
-        <Text>Aditya</Text>
-      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(_, index) => index.toString()}
+        ListHeaderComponent={
+          <>
+            {/* Avatar or user Image */}
+            <View style={styles.AvatarContainer}>
+              <Avatar
+                name="user"
+                size={hp(25)}
+                imageUrl={require("../../../assets/boy.jpg")}
+              />
+              <Text style={styles.userName}>Aditya Raj</Text>
+            </View>
+            {/* Edit buttom */}
+            <View style={styles.editContainer}>
+              <Pressable style={styles.editButton}>
+                <Text style={styles.buttonText}>Edit Profile</Text>
+              </Pressable>
+            </View>
+            {/* dividing line */}
+            <View style={styles.line} />
+
+            {/* Follower and Following */}
+            <View style={styles.FandFContainer}>
+              {/* followers */}
+              <View
+                style={{
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.FAndFContentTextOne}>2,739</Text>
+                <Text style={styles.FAndFContentTextTwo}>Followers</Text>
+              </View>
+              {/* Following */}
+              <View
+                style={{
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.FAndFContentTextOne}>347</Text>
+                <Text style={styles.FAndFContentTextTwo}>Following</Text>
+              </View>
+            </View>
+            {/* dividing line */}
+            <View style={styles.line} />
+            {/* Playlists */}
+            <View style={styles.TrendingItem}>
+              <Text style={styles.TrendingTextTitle}>Playlists</Text>
+              <Pressable onPress={() => navigation.navigate("Playlists")}>
+                <Text style={styles.TextSeeAll}>See All</Text>
+              </Pressable>
+            </View>
+          </>
+        }
+        renderItem={({ item }) => {
+          // console.log('item--> ',item.song)
+          return (
+            <>
+              <MusicShowcase
+                imgUrl={item.image}
+                singerName={item.song}
+                songName={item.singer}
+              />
+            </>
+          );
+        }}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        contentContainerStyle={styles.listContainer}
+        columnWrapperStyle={styles.row}
+      />
     </SafeAreaView>
   );
 };
@@ -59,13 +142,89 @@ const styles = StyleSheet.create({
     fontSize: wp(7),
     paddingRight: 12,
   },
-  AvatarContainer:{
+  AvatarContainer: {
     // borderWidth:2,
-    flexDirection:'column',
+    flexDirection: "column",
     // justifyContent:'center',
-    alignItems:'center',
-    marginTop:hp(5),
-  }
+    alignItems: "center",
+    marginTop: hp(5),
+  },
+  userName: {
+    fontFamily: fonts.Regular,
+    fontSize: hp(4),
+    marginTop: hp(1.2),
+    fontWeight: theme.fontWeight.medium,
+  },
+  editContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: hp(2),
+  },
+  editButton: {
+    borderWidth: 1.5,
+    padding: 7,
+    borderRadius: 100,
+    borderColor: theme.colors.green,
+  },
+  buttonText: {
+    fontFamily: fonts.SemiBold,
+    fontSize: hp(1.8),
+    fontWeight: theme.fontWeight.medium,
+    color: theme.colors.green,
+    paddingHorizontal: 7,
+    paddingVertical: 2,
+  },
+  line: {
+    borderWidth: 0.7,
+    marginHorizontal: 15,
+    borderColor: theme.colors.neutral(0.3),
+    borderRadius: 100,
+    borderCurve: "continuous",
+  },
+  FandFContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginVertical: hp(1.4),
+  },
+  FAndFContentTextOne: {
+    padding: 3,
+    fontFamily: fonts.SemiBold,
+    fontWeight: theme.fontWeight.semiBold,
+    fontSize: hp(3),
+    color: theme.colors.black,
+  },
+  FAndFContentTextTwo: {
+    fontFamily: fonts.Regular,
+    fontWeight: theme.fontWeight.medium,
+    fontSize: wp(4.6),
+    color: theme.colors.neutral(0.4),
+  },
+  listContainer: {
+    paddingHorizontal: 10,
+  },
+  row: {
+    justifyContent: "space-around",
+  },
+  TrendingItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: wp(3),
+    // borderWidth: 2,
+  },
+  TrendingTextTitle: {
+    fontFamily: fonts.Regular,
+    fontSize: wp(6.1),
+    fontWeight: theme.fontWeight.semiBold,
+    paddingVertical: 14,
+  },
+  TextSeeAll: {
+    fontFamily: fonts.Regular,
+    fontWeight: theme.fontWeight.medium,
+    fontSize: wp(4.2),
+    color: theme.colors.green,
+  },
 });
 
 export default UserInfo;
